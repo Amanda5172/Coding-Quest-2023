0ecdd278d08ae5e6054d9ef7d01e35442f669fd9
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2023-03-14 23:30:08 +0800

    Update MAR6_INVENTORY_CHECK.py

--- MAR6_INVENTORY_CHECK.py
+++ MAR6_INVENTORY_CHECK.py
@@ -1,1 +1,30 @@
 
+item=[]
+quantity=[]
+
+with open("input.txt","r") as ef:
+    t=ef.readlines()
+    for k in range(len(t)):
+        x,y,z=t[k].split()
+
+        if z not in item:
+            item.append(z)
+            quantity.append(int(y))
+        else:
+            r=item.index(z)
+            quantity[r]=quantity[r]+int(y)
+
+        
+        
+
+print(item)
+print(quantity)
+
+
+mod=1
+
+for i in range(len(quantity)):
+    mod = mod * (quantity[i] % 100)
+
+print(mod)
+
