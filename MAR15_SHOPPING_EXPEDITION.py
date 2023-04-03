f8705fc9c0e6b6876d9fdc2af2624d5c5d1abb31
Amanda5172 <112593829+Amanda5172@users.noreply.github.com>
2023-04-04 07:43:14 +0800

    Update MAR15_SHOPPING_EXPEDITION.py

--- MAR15_SHOPPING_EXPEDITION.py
+++ MAR15_SHOPPING_EXPEDITION.py
@@ -1,1 +1,26 @@
+#get permutation
+#get distance
 
+#function to sort data?
+import math
+
+l=[]
+with open("input.txt") as ef:
+    r=ef.readlines()
+    for k in range(len(r)):
+        l.append(r[k].split())
+
+print(l)
+
+
+
+def distance(p1,p2):
+    d=l[p1-1][p2-1]
+    return d
+
+
+start=1
+placesbeen=[1]
+dist=0
+distlist=[]
+n=0
